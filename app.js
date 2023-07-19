efc9dcde2dbd7dc0fa8b4414745c8c3b8eaebe08
TranspoLord <ascatlover9@gmail.com>
2023-07-19 13:45:37 -0500

    First vue app

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+  data: function() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },  
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    }    
+  }
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
+}).mount('#app');
 
-buttonEl.addEventListener('click', addGoal);
+
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
