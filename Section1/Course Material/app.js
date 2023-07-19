01447f68af69306f2c28e70b5c35cb7e311ef1b9
TranspoLord <ascatlover9@gmail.com>
2023-07-19 15:49:04 -0500

    Updating files for new course material

--- Section1/Course Material/app.js
+++ Section1/Course Material/app.js
@@ -1,29 +1,9 @@
 const app = Vue.createApp({
-    data() {  // data contains all the data that we want to use in this component
-        return {
-            courseGoalA: 'Finish the course and learn Vue!',
-            courseGoalB: '<h2>Master Vue and build amazing apps!</h2>',
-            vueLink: 'https://vuejs.org/',
-
-            // Showing how to create vars
-            var1: 'var1',
-            var2: 2,
-            var3: true
-        };
-    },
-    methods: { // methods contains all the functions that we want to use in this component
-        outputGoal() {
-            const randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                return this.courseGoalA;
-            } else {
-                return this.courseGoalB;
-            }
-        }
-
-    }
-
-
+  data() {
+    return {
+      counter: 0,
+    };
+  },
 });
 
-app.mount('#user-goal')
+app.mount('#events');
