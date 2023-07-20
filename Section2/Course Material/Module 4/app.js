c28af3e42353a55e823a4d16be47329be8186b86
TranspoLord <ascatlover9@gmail.com>
2023-07-19 23:28:23 -0500

    Adding module 4 and assignment 3. Learned watch and computed properties

--- Section2/Course Material/Module 4/app.js
+++ Section2/Course Material/Module 4/app.js
@@ -2,12 +2,41 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      fullname: ''
     };
   },
-  methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+  computed: {
+    fullname() { // computed properties are called when data is called
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Lastname';
+    }
+  },
+  watch: { // watchers are called everytime the data values change
+    // name() {
+    //     this.fullname = this.name + ' ' + 'Lastname';
+    // }
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function() {
+          that.counter = 0;
+        }, 2000);
+      }
+    }
+
+  },
+  methods: { // methods are called like functions
+    outputFullname() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Lastname';
+    },
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,6 +44,9 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
     }
   }
 });
