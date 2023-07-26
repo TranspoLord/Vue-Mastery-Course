168650aa24559b2641cad12447a32a9cdc9a91ed
TranspoLord <ascatlover9@gmail.com>
2023-07-25 22:15:13 -0500

    One app vs Multiple Apps

--- Section5/Module1/app.js
+++ Section5/Module1/app.js
@@ -16,3 +16,31 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteMeal: 'Pizza',
+    }
+  },
+});
+
+app2.component('#app');
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!',
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World!';
+    }
+    target.message = value;
+  }
+};
+
+const proxy = new Proxy(data, handler);
+proxy.message = 'Hello!!!!';
+console.log(proxy.longMessage);
